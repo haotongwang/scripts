44b313bb604bea8438677c479eb8cb108e9eff6e
Nick Wang <nicholas.wang.h@gmail.com>
2021-08-28 10:43:23 +1000

    meta

--- youtube-dl-rename.py
+++ youtube-dl-rename.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import re
 import subprocess
 import sys
 from glob import glob
@@ -11,34 +12,51 @@ __dirname__ = path.dirname(path.realpath(__file__))
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('name', type=str)
-parser.add_argument('url', type=str)
-parser.add_argument('music', type=str)
+parser.add_argument('music', type=str, nargs='?')
 parser.add_argument('others', nargs='*')
-parser.add_argument('-m', action='store_false', help='Skip downloading')
 
 args = parser.parse_args()
 
-name_path: str = args.name
-url_path: str = args.url
-music_dir: str = args.music
+MUSIC_DIR = path.join(os.environ.get("USERPROFILE"), "Music/youtube-dl")
 
-with open(name_path, encoding='utf8') as name_file:
-  names = name_file.readlines()
-  with open(url_path, encoding='utf8') as url_file:
-    urls = url_file.readlines()
+# if args.music is not None:
+#   # Split names and urls
+#   with open(args.music, encoding="utf-8") as f:
+#     pass
 
-    for (name, url) in zip(names, urls):
-      name = name.strip().strip('\n')
-      url = url.strip().strip('\n')
+#   # Create Music directory and delete contents
+#   try:
+#     os.mkdir(MUSIC_DIR, 0o777)
+#   except FileExistsError:
+#     for f in glob(path.join(MUSIC_DIR, ".*")):
+#       os.remove(f)
 
-      # Download
-      if args.m:
-        print("Downloading")
-        subprocess.run(["youtube-dl", "-x", "--audio-quality", "0",
-                        "--audio-format", "mp3", "--add-metadata", "-o", f"{music_dir}/{name}.%%(ext)s", url])
+#   # Download
+#   for (name, url) in zip(names, urls):
+#     name = name.strip().strip('\n')
+#     url = url.strip().strip('\n')
+
+#     # Download
+#     if not args.m:
+#       print("Downloading")
+#       subprocess.run(["youtube-dl", "-x", "--audio-quality", "0",
+#                       "--audio-format", "mp3", "--add-metadata", "-o", f"{MUSIC_DIR}/{name}.%%(ext)s", url])
+
+artistSplit = re.compile(r"[&,]")
 
 # Metadata
-for f in glob(path.join(music_dir, '*.mp3')):
+for f in glob(path.join(MUSIC_DIR, '*.mp3')):
   audio = eyed3.load(f)
-  print(audio.tag)
+
+  # Extract artist and title
+  with open(f) as f_:
+    [artist, title] = path.basename(f_.name).strip('.mp3').split(" - ")
+    artist = artistSplit.sub("/", artist)
+    title = title.strip().strip('[replace]').strip()
+    print(artist)
+
+    # Write metadata
+    audio.tag.artist = artist
+    audio.tag.title = title
+
+    audio.tag.save()
